70168d5e2d50a5a234a63c6fd11f36713531b645
Ioan <ioan.zicu94gmail.com>
2020-10-04 12:23:32 +0200

    Get student by id

--- spring-rest-demo/src/main/java/com/ioan/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/ioan/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.ioan.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +16,36 @@ import com.ioan.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
+	private List<Student> theStudents;
+
+	// define @PostConstruct to load the student data ... only once!
+
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
+
+		theStudents.add(new Student("Sonia", "Kage"));
+		theStudents.add(new Student("Johny", "Kage"));
+		theStudents.add(new Student("Shau", "Khan"));
+	}
+
 	// define endpoint for "/students" - return list of students
 
 	@GetMapping("/students")
 	public List<Student> getStudent() {
 
-		List<Student> theStudents = new ArrayList<>();
-
-		theStudents.add(new Student("Sonia", "Kage"));
-		theStudents.add(new Student("Johny", "Kage"));
-		theStudents.add(new Student("Shau", "Khan"));
-
 		return theStudents;
+	}
+
+	// define endpoint for "/students/{studentId}" - return student at index
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		// just index into the list
+
+		return theStudents.get(studentId);
+
 	}
 }
